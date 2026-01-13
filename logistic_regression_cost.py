6d01c5c4f3bbba59f31b34df630f1ee15ff335d9
kavinda warakamura <warakaofficial@gmail.com>
2026-01-14 03:50:05 +0530

    Initial logistic regression implementation

--- logistic_regression_cost.py
+++ logistic_regression_cost.py
@@ -26,9 +26,59 @@ def compute_cost_logistic(X, y, w , b):
     cost = cost / m
     return cost
 
-w_tmp = np.array([1,1])
-b_tmp = -3
-print(compute_cost_logistic(X_train, y_train, w_tmp, b_tmp))
+#compute gradient 
+def compute_gradient_logistic(X, y, w, b):
+    m,n = X.shape
+    dj_dw = np.zeros((n,))
+    dj_db = 0.0
+
+    for i in range(m):
+        z_i = np.dot(X[i],w) + b
+        f_wb_i = sigmoid(z_i)
+        error_i = f_wb_i - y[i]
+        for j in range(n):
+            dj_dw[j] += error_i * X[i][j]
+        dj_db += error_i
+
+    dj_dw = dj_dw / m
+    dj_db = dj_db / m
+
+    return dj_db, dj_dw
+
+#gradient decent
+def gradient_descent_logistic(X, y, w_in, b_in, alpha, num_iters):
+    w = w_in.copy()
+    b = b_in
+    for i in range(num_iters):
+        dj_db, dj_dw = compute_gradient_logistic(X, y, w, b)
+        w = w - alpha * dj_dw
+        b = b - alpha * dj_db
+    return w, b
+
+
+
+w_tmp  = np.zeros_like(X_train[0])
+b_tmp  = 0.
+alph = 0.1
+iters = 10000
+
+w_out, b_out = gradient_descent_logistic (X_train, y_train, w_tmp, b_tmp, alph, iters) 
+print(f"\nupdated parameters: w:{w_out}, b:{b_out}")
+
+
+
+
+
+
+X_tmp = np.array([[0.5, 1.5], [1,1], [1.5, 0.5], [3, 0.5], [2, 2], [1, 2.5]])
+y_tmp = np.array([0, 0, 0, 1, 1, 1])
+w_tmp = np.array([2.,3.])
+b_tmp = 1.
+dj_db_tmp, dj_dw_tmp = compute_gradient_logistic(X_tmp, y_tmp, w_tmp, b_tmp)
+print(f"dj_db: {dj_db_tmp}" )
+print(f"dj_dw: {dj_dw_tmp.tolist()}" )
+
+
 
 w_array1 = np.array([1,1])
 b_1 = -3
